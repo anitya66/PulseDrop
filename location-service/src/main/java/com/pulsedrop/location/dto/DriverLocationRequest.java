16a0cd40c9886bf3c7f316d4c32498224005ff66
anitya66 <123goldirajput@gmail.com>
2026-09-12 13:48:01 +0530

    fix: validate driver location coordinates

--- location-service/src/main/java/com/pulsedrop/location/dto/DriverLocationRequest.java
+++ location-service/src/main/java/com/pulsedrop/location/dto/DriverLocationRequest.java
@@ -1,7 +1,9 @@
 package com.pulsedrop.location.dto;
 
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.NotNull;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,9 +13,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class DriverLocationRequest {
 
-    @NotNull
+    @NotNull(message = "Latitude is required")
+    @DecimalMin(value = "-90.0", message = "Latitude must be >= -90")
+    @DecimalMax(value = "90.0", message = "Latitude must be <= 90")
     private Double latitude;
 
-    @NotNull
+    @NotNull(message = "Longitude is required")
+    @DecimalMin(value = "-180.0", message = "Longitude must be >= -180")
+    @DecimalMax(value = "180.0", message = "Longitude must be <= 180")
     private Double longitude;
 }
